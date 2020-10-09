ff533d26ad16c3c13caf88e38b238df3bca1fa20
yakmax <yakmax@gmail.com>
2020-10-10 02:03:02 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 200;
+            int b = 200;
+            int operation = Console.Read();
+            if (operation == 43) Console.WriteLine(a + b);
+            if (operation == 45) Console.WriteLine(a - b);
+            //Console.WriteLine(operation);
+            Console.Read();
+        }
+    }
+}
